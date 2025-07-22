3e16daae19aba5b6e3b4dd11346ac79f1466884b
jonathan zollinger <jonathan.zollinger@gmail.com>
2025-07-22 15:15:03 -0600

    add initial "add-ticket" function

--- Invoke-ZendeskApiCall.ps1
+++ Invoke-ZendeskApiCall.ps1
@@ -37,3 +37,70 @@ function Invoke-ZendeskApiCall {
         throw $_
     }
 }
+
+function Add-Ticket {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Subject,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Comment,
+
+        [Parameter(Mandatory=$false)]
+        [string]$RequesterEmail,
+
+        [Parameter(Mandatory=$false)]
+        [string]$RequesterName,
+
+        [Parameter(Mandatory=$false)]
+        [string[]]$Tags,
+
+        [Parameter(Mandatory=$false)]
+        [string]$Type,
+
+        [Parameter(Mandatory=$false)]
+        [string]$Priority
+    )
+
+    $ticketBody = @{
+        "ticket" = @{
+            "subject" = $Subject
+            "comment" = @{
+                "body" = $Comment
+            }
+        }
+    }
+
+    if ($RequesterEmail) {
+        $ticketBody.ticket.requester = @{ "email" = $RequesterEmail }
+        if ($RequesterName) {
+            $ticketBody.ticket.requester.name = $RequesterName
+        }
+    }
+
+    if ($Tags) {
+        $ticketBody.ticket.tags = $Tags
+    }
+
+    if ($Type) {
+        $ticketBody.ticket.type = $Type
+    }
+
+    if ($Priority) {
+        $ticketBody.ticket.priority = $Priority
+    }
+
+    $jsonBody = $ticketBody | ConvertTo-Json -Depth 4
+
+    $url = "$($Global:ZendeskUrl)/api/v2/tickets.json"
+
+    try {
+        $response = Invoke-ZendeskApiCall -Url $url -Method 'POST' -Body $jsonBody
+        return $response
+    }
+    catch {
+        Write-Error "Error adding ticket: $($_.Exception.Message)"
+        throw $_
+    }
+}
