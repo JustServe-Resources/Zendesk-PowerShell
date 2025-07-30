41191dd05c1c3ea216e98af9451489ff7eadef33
jonathan zollinger <jonathan.zollinger@gmail.com>
2025-07-30 10:13:07 -0600

    fix:  return all responses in add-tickets

--- Add-Tickets.ps1
+++ Add-Tickets.ps1
@@ -2,18 +2,19 @@ function Add-Tickets {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [PSCustomObject]
+        [array]
         $JiraTickets
     )
 
     Test-ZendeskEnvironment
 
-    Write-Host "Found $($JiraTickets.Count) tickets to import. Processing in batches of $batchSize."
-
     $endRange = [math]::Min(99, $JiraTickets.Count)
+    Write-Host "Found $($JiraTickets.Count) tickets to import. Processing $($endRange - 1) tickets."
+    $responses = @()
     if ($JiraTickets.Count -lt $endRange) {
-        Add-Tickets $JiraTickets[($endRange + 1) .. ($JiraTickets.Count - 1)]
+        $responses += Add-Tickets $JiraTickets[($endRange + 1) .. ($JiraTickets.Count - 1)]
     }
     $body = @(tickets = $JiraTickets[0..$endRange]) | ConvertTo-Json -Depth 100
-    return Invoke-ZendeskApiCall -UriPath "/api/v2/tickets/create_many.json" -Method 'POST' -Body $body
+    $responses += Invoke-ZendeskApiCall -UriPath "/api/v2/tickets/create_many.json" -Method 'POST' -Body $body
+    return $responses
 }
