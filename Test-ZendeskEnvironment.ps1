47776d198d4c9ceb58c9842fc4795aeaf625cc60
jonathan zollinger <jonathan.zollinger@gmail.com>
2025-07-30 10:24:14 -0600

    fix: checks throw error when checks fail

--- Test-ZendeskEnvironment.ps1
+++ Test-ZendeskEnvironment.ps1
@@ -6,13 +6,20 @@ function Test-ZendeskEnvironment {
     .DESCRIPTION
         This function iterates through a predefined list of environment variables
         (ZendeskEmail, ZendeskApiToken, ZendeskUrl) and verifies if each is set.
-        If any required environment variable is not found, it writes an error
-        message and exits the script with an error code.
+        If any required environment variable is not found, it throws an error.
+
+    .NOTES
+        This function does not return any value. It throws an error if environment variables are not set.
     #>
     foreach ($envVar in @("ZendeskEmail", "ZendeskApiToken", "ZendeskUrl")) {
-        if (-not (Get-Item -Path "Env:$envVar" -ErrorAction SilentlyContinue)) {
-            Write-Error "$envVar environment variable is not set."
-            exit 1
+        $missingVars = @()
+        if (-not (Test-Path -Path "Env:$envVar")) {
+            $missingVars += $envVar
         }
+    }
+
+    if ($missingVars.Count -gt 0) {
+        $errorMessage = "Required environment variable(s) not set: $($missingVars -join ', ')"
+        throw $errorMessage
     }
 }
